42c21688b5e06e65c50ae93f9b744de82930bf71
xsebek02 <petr@peklo.(none)>
2012-03-06 15:32:03 +0100

    Zakladni struktura

---
+++ src/pn/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: petr
+ *
+ * Created on 6. březen 2012, 15:29
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
